91844c6903f5a804fb6ce444032aa89b3482a9f2
MrDave1999 <daveseva2010@hotmail.es>
2022-08-15 00:58:28 -0500

    Added Project files

---
+++ src/CustomBaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Microsoft.EntityFrameworkCore;
+
+public class CustomBaseEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
---
+++ src/CustomDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.EntityFrameworkCore;
+
+public class CustomDbContext : DbContext
+{
+    public virtual DateTime CreatedAt => DateTime.Now;
+    public virtual DateTime UpdatedAt => DateTime.Now;
+
+    public CustomDbContext() { }
+    public CustomDbContext(DbContextOptions options) : base(options) { }
+
+    private IEnumerable<EntityEntry> GetEntityEntries()
+    {
+        var entries = ChangeTracker
+            .Entries()
+            .Where(e => e.Entity is CustomBaseEntity && (
+                    e.State == EntityState.Added ||
+                    e.State == EntityState.Modified));
+        return entries;
+    }
+
+    private void SetCurrentDateOnEachEntity()
+    {
+        foreach (var entityEntry in GetEntityEntries())
+        {
+            ((CustomBaseEntity)entityEntry.Entity).UpdatedAt = UpdatedAt;
+
+            if (entityEntry.State == EntityState.Added)
+            {
+                ((CustomBaseEntity)entityEntry.Entity).CreatedAt = CreatedAt;
+            }
+        }
+    }
+
+    public override int SaveChanges()
+    {
+        SetCurrentDateOnEachEntity();
+        return base.SaveChanges();
+    }
+
+    public override int SaveChanges(bool acceptAllChangesOnSuccess)
+    {
+        SetCurrentDateOnEachEntity();
+        return base.SaveChanges(acceptAllChangesOnSuccess);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+    {
+        SetCurrentDateOnEachEntity();
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SetCurrentDateOnEachEntity();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
